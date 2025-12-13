ffaed248df28168c2c3539a753b4ff5633fb6c74
Hsu Myat Htike <hsumyathtike2004@gmail.com>
2025-12-13 10:51:00 +0630

    programmatic approach for spring bean creation

---
+++ src/main/java/programmatic_bean/Main.java
@@ -0,0 +1,50 @@
+package programmatic_bean;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        (1) first way to try register bean for programmatic way
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot parrot = new Parrot();
+        parrot.setName("Koko");
+
+        Supplier<Parrot> parrotSupplier = () -> parrot;
+
+//        context.registerBean((String) null,
+//                Parrot.class,parrotSupplier);
+
+        // OR //
+       // context.registerBean(Parrot.class, parrotSupplier);
+
+        // OR //
+        context.registerBean("parrot1",Parrot.class, parrotSupplier);
+        Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p.getName());
+        (1) ==========================================
+         */
+        /*
+      */
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        context.registerBean("parrot1", Parrot.class,
+                () -> new Parrot("Ki Ki"),bc -> bc.setPrimary(true));
+        /*
+         bc -> bc.setPrimary(true) is “Make this bean the default Parrot bean Spring should use.”
+         It's from interface of BeanDefinitionCustomizer
+         */
+
+        context.registerBean("parrot2", Parrot.class,
+                () -> new Parrot("Ko Ko"));
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+    }
+}
---
+++ src/main/java/programmatic_bean/ProjectConfig.java
@@ -0,0 +1,7 @@
+package programmatic_bean;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+}
